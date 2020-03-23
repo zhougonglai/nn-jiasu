76c80904f738f1d6a0c07168fd494e75359c2359
公来 周 <zhougonglai@nn.com>
2020-03-23 15:20:23 +0800

    type: sign api

--- src/services/sign.js
+++ src/services/sign.js
@@ -2,35 +2,37 @@ import Request from '@utils/request';
 
 export default class SignService {
 	constructor() {
-		this.request = new Request(process.env.VUE_APP_BASE);
+		this.request = new Request(
+			process.env.VUE_APP_BASE + process.env.VUE_APP_JIASU_NAMESPACE,
+		);
 	}
 
 	async login(sign) {
-		return await this.request.post('api/login', sign);
+		return await this.request.post('/api/login', sign);
 	}
 
 	async register(sign) {
-		return await this.request.post('api/register', sign);
+		return await this.request.post('/api/register', sign);
 	}
 
 	async smscode(data) {
-		return await this.request.post('tools/send/smscode', data);
+		return await this.request.post('/tools/send/smscode', data);
 	}
 
 	async validate(data) {
-		return await this.request.post('tools/validate/sms', data);
+		return await this.request.post('/tools/validate/sms', data);
 	}
 
 	async loginByCode(sign) {
-		return await this.request.post('api/login/mobile', sign);
+		return await this.request.post('/api/login/mobile', sign);
 	}
 
 	async signBind(sign) {
-		return await this.request.post('api/open/bind', sign);
+		return await this.request.post('/api/open/bind', sign);
 	}
 
 	async forgotPassword(sign) {
-		return await this.request.post('api/password/forgot', sign);
+		return await this.request.post('/api/password/forgot', sign);
 	}
 
 	async geetest() {
